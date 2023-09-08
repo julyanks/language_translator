ff5c314e12eff8fe65d558e18948c4ee09a0fd7a
julyanks <97847850+julyanks@users.noreply.github.com>
2023-09-08 23:28:37 +0530

    Add files via upload

---
+++ js/countries.js
@@ -0,0 +1,16 @@
+const countries = {
+    "ar-SA": "Arabic",
+    "bn-IN": "Bengali",
+    "de-DE": "German",
+    "el-GR": "Greek",
+    "en-GB": "English",
+    "es-ES": "Spanish",
+    "fr-FR": "French",
+    "gu-IN": "Gujarati",
+    "hi-IN": "Hindi",
+    "it-IT": "Italian",
+    "ja-JP": "Japanese",
+    "ko-KR": "Korean",
+    "ta-LK": "Tamil",
+    "te-IN": "Telugu",
+}
